24dfaf158bbee26f71e24d5ae677ce4e7d22d485
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-02-24 20:17:45 +0000

    scroll down

--- react-portfolio/src/components/Header/Home/ScrollDown.jsx
+++ react-portfolio/src/components/Header/Home/ScrollDown.jsx
@@ -4,7 +4,10 @@ export default function ScrollDown() {
   return (
     <div className="home__scroll">
       <a href="" className="home__scroll-button button--flex">
-        <img src={scrollSVG} alt="Scroll button" />
+        {/* <img src={scrollSVG} alt="Scroll button" /> */}
+        <i class="bx bx-mouse"></i>
+        <span className="home__scroll-name">Scroll Down</span>
+        <i class="bx bx-down-arrow-alt home__scroll-arrow"></i>
       </a>
     </div>
   );
