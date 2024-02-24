e830b2e3f2d65ed8d533aa298f550bc0406e3965
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-02-24 15:33:42 +0000

    starting home

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header/Header";
 import "./App.css";
+import Home from "./components/Header/Home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <main className="main">
+        <Home />
+      </main>
     </>
   );
 }
