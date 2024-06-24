ae83003656f6619046aab70ccda84759c9d6923b
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-06-25 00:48:36 +0100

    hero

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,10 @@ export default function Projects() {
         <div className={styles.projectInfo}>
           <h2>Gains GPT</h2>
           <div className={styles.projectLinks}>
-            <a href="https://gains-gpt.netlify.app" target="_blank">
+            <a
+              href="https://github.com/MIbrahim71/React-Gym-App "
+              target="_blank"
+            >
               <svg
                 stroke="currentColor"
                 fill="none"
@@ -24,10 +27,7 @@ export default function Projects() {
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
               </svg>
             </a>
-            <a
-              href="https://github.com/MIbrahim71/React-Gym-App"
-              target="_blank"
-            >
+            <a href="https://gains-gpt.netlify.app" target="_blank">
               <svg
                 stroke="currentColor"
                 fill="none"
@@ -55,7 +55,10 @@ export default function Projects() {
         <div className={styles.projectInfo}>
           <h2>Modern Bank Landing Page</h2>
           <div className={styles.projectLinks}>
-            <a href="https://gains-gpt.netlify.app" target="_blank">
+            <a
+              href="https://github.com/MIbrahim71/React-Gym-App"
+              target="_blank"
+            >
               <svg
                 stroke="currentColor"
                 fill="none"
@@ -70,10 +73,7 @@ export default function Projects() {
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
               </svg>
             </a>
-            <a
-              href="https://github.com/MIbrahim71/React-Gym-App"
-              target="_blank"
-            >
+            <a href="https://gains-gpt.netlify.app" target="_blank">
               <svg
                 stroke="currentColor"
                 fill="none"
@@ -101,7 +101,7 @@ export default function Projects() {
         <div className={styles.projectInfo}>
           <h2>Project 3</h2>
           <div className={styles.projectLinks}>
-            <a href="https://gains-gpt.netlify.app" target="_blank">
+            <a href="" target="_blank">
               <svg
                 stroke="currentColor"
                 fill="none"
@@ -116,10 +116,7 @@ export default function Projects() {
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
               </svg>
             </a>
-            <a
-              href="https://github.com/MIbrahim71/React-Gym-App"
-              target="_blank"
-            >
+            <a href="" target="_blank">
               <svg
                 stroke="currentColor"
                 fill="none"
